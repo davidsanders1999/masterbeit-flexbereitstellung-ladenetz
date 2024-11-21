65ca888937264351092b3e69cc44202366cc698f
davidsanders1999 <david.sanders@online.de>
2024-11-21 11:59:16 +0100

    Lastgang

---
+++ config.py
@@ -0,0 +1,19 @@
+
+start_date = '2023-01-01'
+end_date = '2023-01-02'
+freq = 30
+
+anzahl_ladesaeulen = {
+    'LPC': 10, # PKW Charging
+    'HPC': 5,
+    'NCS': 5, # LKW Charging
+    'MWC': 5
+}
+
+max_leistung_ladesaeulen = {
+    'typ_ladesaule': 'max_leistung_ladesaeulen',
+    'LPC': 50, # PKW Charging
+    'HPC': 150,
+    'NCS': 350, # LKW Charging
+    'MWC': 500
+}
---
+++ main.py
@@ -0,0 +1,117 @@
+import pandas as pd
+import numpy as np
+import os
+from datetime import datetime
+
+import config
+
+# Erstellen Sie ein DataFrame mit den Spalten 'Zeitspanne', 'Ladesäule', 'energie' und 'leistung'
+start_date = pd.to_datetime(config.start_date)
+end_date = pd.to_datetime(config.end_date)
+
+date_range = pd.date_range(start=start_date, end=end_date, freq=f"{config.freq}T")
+dict_lastgang = []
+for date in date_range:
+    for typ_ladesaule, anzahl_ladesaeule in config.anzahl_ladesaeulen.items():
+        for i in range(anzahl_ladesaeule):
+            max_leistung = config.max_leistung_ladesaeulen[typ_ladesaule]
+            max_energie = max_leistung * config.freq / 60
+            leistung = None
+            energie = None
+            dict_lastgang.append({'Zeitspanne': date, 'Ladesäule': f'{typ_ladesaule}_{i}', 'max_energie':max_energie, 'max_leistung':max_leistung, 'energie': energie, 'leistung': leistung})
+
+df_lastgang = pd.DataFrame(dict_lastgang)
+
+# Erstellen Sie ein DataFrame mit den Spalten 'Zeitspanne' und 'Kapazität'
+df_lkws_ankommen = pd.DataFrame(columns=['Zeitspanne', 'Kapazität','Ladezustand'])
+dict_lkws = {
+    'Zeitspanne': ['2023-01-01 11:00:00','2023-01-01 11:00:00', '2023-01-01 16:00:00', '2023-01-01 19:00:00'],
+    'Kapazität': [1000,1000, 50, 50],
+    'Ladezustand': [0.1,0.1, 0.1, 0.1],
+    'typ_ladesaeule': ['MWC','MWC', 'NCS', 'NCS']
+}
+
+dict_lkws['Zeitspanne'] = [datetime.strptime(zeit, '%Y-%m-%d %H:%M:%S') for zeit in dict_lkws['Zeitspanne']] # Konvertieren Sie die Zeitwerte in datetime-Objekte
+df_lkws_ankommen = pd.DataFrame(dict_lkws)
+df_lkws_charging = pd.DataFrame()
+
+for index_lkw, row_lkw in df_lkws_ankommen.iterrows(): # Iterieren Sie über die ankommenden LKWs des DataFrames df_lkws
+    startzeit = row_lkw['Zeitspanne']
+    lkw_ladezustand = row_lkw['Ladezustand']
+    lkw_kapazität = row_lkw['Kapazität']
+    lkw_ladetyp = row_lkw['typ_ladesaeule']
+
+    for index_lastgang, row_lastgang in df_lastgang.iterrows(): # Finden einer freien Ladesäule
+        if row_lastgang['Ladesäule'].split('_')[0] == lkw_ladetyp and row_lastgang['leistung'] == None and row_lastgang['Zeitspanne'] == startzeit:
+            # Hier können Sie den gefundenen Eintrag weiter verarbeiten
+            ladesaeule = row_lastgang['Ladesäule']
+            ladesauele_max_energie = row_lastgang['max_energie']
+            ladesauele_max_leistung = row_lastgang['max_leistung']
+            break
+    
+    while lkw_ladezustand < 0.8:
+        if lkw_kapazität < ladesauele_max_energie:
+            df_lastgang.loc[(df_lastgang['Zeitspanne'] == startzeit) & (df_lastgang['Ladesäule'] == ladesaeule), 'energie'] = lkw_kapazität
+            df_lastgang.loc[(df_lastgang['Zeitspanne'] == startzeit) & (df_lastgang['Ladesäule'] == ladesaeule), 'leistung'] = lkw_kapazität * 60 / config.freq
+            lkw_ladezustand = 1
+        else:
+            df_lastgang.loc[(df_lastgang['Zeitspanne'] == startzeit) & (df_lastgang['Ladesäule'] == ladesaeule), 'energie'] = ladesauele_max_energie
+            df_lastgang.loc[(df_lastgang['Zeitspanne'] == startzeit) & (df_lastgang['Ladesäule'] == ladesaeule), 'leistung'] = ladesauele_max_leistung
+            lkw_ladezustand = lkw_ladezustand + ladesauele_max_energie / lkw_kapazität
+            startzeit = startzeit + pd.Timedelta(minutes=config.freq)
+        
+
+
+print(df_lastgang.loc[df_lastgang['energie'].notnull()])
+
+# Speichern Sie das DataFrame in eine CSV-Datei
+if not os.path.exists('./Output'):
+    os.makedirs('./Output')
+
+df_lastgang.to_csv('./Output/Lastprofil.csv')
+df_lkws_ankommen.to_csv('./Output/LKWs.csv')
+'''
+df_lastgang = pd.DataFrame(columns=['Zeitspanne', 'Ladesäule', 'energie', 'leistung'])
+
+for date in date_range:
+    for typ_ladesaule, anzahl_ladesaeule in config.anzahl_ladesaeulen.items():
+        for i in range(anzahl_ladesaeule):
+            leistung = config.max_leistung_ladesaeulen[typ_ladesaule]
+            energie = leistung * config.freq / 60
+            data.append({'Zeitspanne': date, 'Ladesäule': f'{typ_ladesaule}_{i}', 'energie': energie, 'leistung': leistung})
+
+
+
+lastgang = {'Zeitspanne': None, 'Ladesäule': None, 'energie': None, 'leistung': None}
+lastgang['Zeitspanne'] = [date.strftime('%Y-%m-%d %H:%M:%S') for date in date_range]
+
+print(lastgang)
+
+lkws = {
+    'Zeitspanne': ['2023-01-01 11:00:00', '2023-01-01 15:00:00', '2023-01-01 19:00:00'],
+    'Kapazität': [50, 50, 50]
+}
+
+# Konvertieren Sie die Zeitwerte in datetime-Objekte
+lastgang['Zeitspanne'] = [datetime.strptime(zeit, '%Y-%m-%d %H:%M:%S') for zeit in lastgang['Zeitspanne']]
+lkws['Zeitspanne'] = [datetime.strptime(zeit, '%Y-%m-%d %H:%M:%S') for zeit in lkws['Zeitspanne']]
+
+for zeitspanne in lastgang['Zeitspanne']:
+    if zeitspanne in lkws['Zeitspanne']:
+        print(zeitspanne)
+    
+
+
+
+
+
+
+
+
+
+
+print(df_lastgang)
+
+
+
+'''
